f4b3a55215d224500674f4066afd0c57e6a0d2c1
Babailan <babailanxx@gmail.com>
2026-06-17 14:45:22 +0800

    initial commit

---
+++ frontend/src/routes/+layout.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+	import favicon from '$lib/assets/favicon.svg';
+
+	let { children } = $props();
+</script>
+
+<svelte:head>
+	<link rel="icon" href={favicon} />
+</svelte:head>
+<main>
+	{@render children()}
+</main>
+
+<style>
+	:global(*) {
+		margin: 0;
+		padding: 0;
+	}
+	:global(html) {
+		background-color: white;
+	}
+	
+</style>
---
+++ frontend/src/routes/+page.svelte
@@ -0,0 +1,7 @@
+<script>
+	export const prerender = true;
+	export const ssr = false;
+</script>
+
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="/test">svelte.dev/docs/kit</a> to read the documentation</p>
