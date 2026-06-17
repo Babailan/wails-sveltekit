228f2a80ab261a2523a70bfc5d33fd985f0f39dd
Ronnel <83863770+Babailan@users.noreply.github.com>
2026-06-17 15:18:39 +0800

    update title

--- main.go
+++ main.go
@@ -17,7 +17,7 @@ func main() {
 
 	// Create application with options
 	err := wails.Run(&options.App{
-		Title:  "film-cast",
+		Title:  "wails-sveltekit",
 		Width:  1024,
 		Height: 768,
 		AssetServer: &assetserver.Options{
